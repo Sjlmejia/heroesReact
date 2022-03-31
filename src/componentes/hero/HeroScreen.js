ae642f6c8ce13e562ab29ffa868d75b09e6ad05e
Jorge Mejia <jorgemejia2304@gmail.com>
2022-03-31 14:06:48 -0500

    List heroes

--- src/componentes/hero/HeroScreen.js
+++ src/componentes/hero/HeroScreen.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useParams, Navigate, useNavigate } from "react-router-dom"
+import { useMemo } from "react";
+import { getHerobyId } from "../../selectors/getHeroeById";
 
 export const HeroScreen = () => {
+  const {heroId} = useParams();
+  const hero = useMemo(() =>getHerobyId(heroId), [heroId]);
+  const navigate = useNavigate()
+  if(!hero) {
+    return <Navigate to ='/' />
+  }
+
+  const {
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters
+  } = hero
+  const imagePath =`/assets/${heroId}.jpg`;
+  const handleReturn = () => {
+    navigate(-1);
+  }
   return (
-    <div>
-      <h1>Heroe</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={ imagePath}
+          alt={superhero}
+          className="img-thumbnail animate__animated animate__fadeInLeft"
+        />
+      </div>
+      <div className="col-8">
+      <h3>{hero.superhero}</h3>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item"><b>Alter ego:</b> {alter_ego}</li>
+        <li className="list-group-item"><b>Publisher:</b> {publisher}</li>
+        <li className="list-group-item"><b>First Appearance:</b> {first_appearance}</li>
+      </ul>
+      <h5>Characters</h5>
+      <p>{ characters }</p>
+      <button 
+        className="btn btn-outline-info"
+        onClick={ handleReturn }
+      >
+        Regresar
+      </button>
+      </div>
     </div>
   )
 }
--- src/componentes/search/SearchScreen.js
+++ src/componentes/search/SearchScreen.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import { useNavigate, useLocation } from 'react-router-dom';
+import querString from 'query-string';
+
+
+import { getHeroesByName } from '../../selectors/getHeroesByName';
+import { HeroCard } from '../hero/HeroCard';
+import { useForm } from '../hooks/useForm';
+import { useMemo } from 'react';
 
 export const SearchScreen = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { q ='' } =querString.parse(location.search);
+  const [state, handleInputChange]=useForm({
+    searchText:q
+  });
+  const {searchText} =state;
+  const heroes =useMemo(() => getHeroesByName(q), [q]) ;
+  const handleSearch= (e) => {
+    e.preventDefault();
+    console.log(searchText)
+    navigate(`?q=${ searchText }`)
+  }
   return (
     <div>
-      <h1>SearchScreen</h1>
+      <h1>Busquedas</h1>
+      <hr/>
+      <div className="row">
+        
+        <div className="col-5">
+          <h4>Buscar</h4>
+          <hr/>
+
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="Buscar un heroe"
+              className="form-control"
+              autoComplete="off"
+              name= "searchText"
+              value={ searchText}
+              onChange={handleInputChange}
+            />
+            <button className="btn btn-outline-primary mt-1" type="submit1">
+              Buscar
+            </button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Resultados</h4>
+          <hr />
+          {
+            (q === '')
+              ?  <div className="alert alert-info">Buscar un heroe</div>
+              : (heroes.length === 0) && <div className="alert alert-danger">No hay resultados: {q} </div>
+          }
+          {
+            heroes.map(hero=>(
+              <HeroCard 
+                key={hero.id}
+                {...hero}
+              />
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
